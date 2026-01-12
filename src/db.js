384fc93ff24243636460154641149bf166833eba
Berkay3734 <berkay.gameofbilisim@gmail.com>
2026-01-12 16:08:41 +0300

    first

--- src/db.js
+++ src/db.js
@@ -2,9 +2,19 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
+  connectTimeout: 20000,
 });
 
 module.exports = pool;
