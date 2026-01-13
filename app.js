c3055f19648e5be84458f19fa28b8547e8059894
Berkay3734 <berkay.gameofbilisim@gmail.com>
2026-01-13 18:18:12 +0300

    appa

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port", PORT);
+});
