3497d7b04ef7c22285e6eae369b917035b87309b
Berkay3734 <berkay.gameofbilisim@gmail.com>
2026-01-13 18:04:16 +0300

    app

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
 require("dotenv").config();
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 5000;
-
-const HOST = process.env.RENDER ? "0.0.0.0" : undefined;
-
-app.listen(PORT, HOST, () => {
-  console.log(`🚀 Backend running on ${HOST || "default"}:${PORT}`);
-});
+module.exports = app;
